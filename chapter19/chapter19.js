a515de539f898b59db5358c5cd9afd30fe4540ce
Bahle Gaji <107406476+Bahle-Gaji@users.noreply.github.com>
2023-02-04 14:51:38 +0200

    Add files via upload

---
+++ chapter19/chapter19.js
@@ -0,0 +1,78 @@
+//create days of week array
+var days = ["Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday"
+];
+//define types of weather
+var Weather = ["Sunny", "Partly Sunny", "Partly Cloudy", "Cloudy", "Raining", "Snowing", "Thunderstorm", "Foggy"];
+
+//set min and max temperature
+var maxTemp = 100;
+var minTemp = 0;
+
+//cost (to you) of a cup of lemonade
+var lemonadeCost = 0.5;
+
+//array for storing daily temps
+var dailyTemp = [];
+
+document.getElementById('OpenTheStand').addEventListener('click', openTheStand)
+
+/**
+generates weather for the week
+**/
+function generateWeather() {
+    var weatherToday;
+    var tempToday;
+
+    for (i = 0; i < days.length; i++) {
+        weatherToday = Weather[Math.floor(Math.random() * Weather.length)];
+        tempToday = Math.floor(Math.random() * (maxTemp - minTemp) + minTemp)
+        dailyTemp[i] = tempToday
+        document.getElementById("5DayWeather").innerHTML += "<div id='" + days[i] + "' class='" + weatherToday + "'><b>Forecast for " + days[i] + ":</b><br><br>" + weatherToday + " and " + tempToday + " degrees.<div > ";
+
+    }
+};
+
+generateWeather();
+
+//calculates glasses of lemonade sold
+function openTheStand() {
+    var glassesSold = 0; // daily
+    var totalGlasses = 0; // weekly
+    var glassesLeft = 0; // left to sell
+    resetForm();
+
+    var numGlasses = Number(document.getElementById('numGlasses').value);
+    var glassPrice = Number(document.getElementById("glassPrice").value);
+
+    for (var i = 0; i < days.length; i++) {
+        glassesSold = Math.floor(dailyTemp[i] / glassPrice);
+        glassesLeft = numGlasses - totalGlasses;
+
+        if (glassesSold > glassesLeft) {
+            glassesSold = glassesLeft;
+        }
+
+        totalGlasses = glassesSold + totalGlasses;
+
+        document.getElementById("result").innerHTML += "<p>" + days[i] + ", you sold " + glassesSold + " glasses of lemonade.</p>";
+    };
+
+    displayResults(numGlasses, glassPrice, totalGlasses);
+};
+
+function resetForm() {
+    document.getElementById("result").innerHTML = "";
+};
+
+function displayResults(weeklyInventory, glassPrice, weeklySales) {
+    var revenue = weeklySales * glassPrice;
+    var expense = weeklyInventory * lemonadeCost;
+    var leftOver = weeklyInventory - weeklySales;
+    var profit = revenue - expense;
+
+    document.getElementById("result").innerHTML += "<p>You sold a total of " + weeklySales + " glasses of lemonade this week.</p>";
+    document.getElementById("result").innerHTML += "<p>Total revenue: $" + revenue + ".</p>";
+    document.getElementById("result").innerHTML += "<p>You have " + leftOver + " glasses of lemonade left over.</p>";
+    document.getElementById("result").innerHTML += "<p>Each glass costs you $" + lemonadeCost + ".Your profit was $" + profit + ".</p>";
+};
